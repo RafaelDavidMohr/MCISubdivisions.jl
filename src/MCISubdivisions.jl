e7a78782018b01ed77c3d8d3430ce062f68f1229
Rafael Mohr <rafaeldavid.mohr@kuleuven.be>
2026-06-25 12:12:00 +0200

    add docstrings

--- src/MCISubdivisions.jl
+++ src/MCISubdivisions.jl
@@ -15,6 +15,13 @@ export mixed_volume, mixed_subdivision, real_root_count, get_eliminant_polytope,
 
 # --- Main functions --- #
 
+"""
+    mixed_volume(F::Vector{<:MPolyRingElem})
+
+Compute the mixed volume of the engineered complete intersection
+defined by (V, A) where A is the support and V the coefficient matrix
+of `F`.
+"""
 function mixed_volume(F::Vector{<:MPolyRingElem})
     R = parent(first(F))
     @assert ngens(R) == length(F) "Input system not square"
@@ -23,6 +30,13 @@ function mixed_volume(F::Vector{<:MPolyRingElem})
     return mixed_volume(A, Vp)
 end
 
+"""
+    mixed_subdivision(F::Vector{<:MPolyRingElem})
+
+Compute a mixed subdivision at a randomly generated height vector of
+the engineered complete intersection defined by (V, A) where A is the
+support and V the coefficient matrix of `F`.
+"""
 function mixed_subdivision(F::Vector{<:MPolyRingElem})
     
     R = parent(first(F))
@@ -33,6 +47,12 @@ function mixed_subdivision(F::Vector{<:MPolyRingElem})
     return A, mixed_subdivision(A, Vp, d)[2]
 end
 
+"""
+    mixed_volume(A::Matrix{Int}, V::Matrix{C}) where C
+
+Compute the mixed volume of the engineered complete intersection
+defined by `V` and `A`.
+"""
 function mixed_volume(A::Matrix{Int}, V::Matrix{C}) where C
 
     d = rand(-LSIZE:LSIZE, size(A, 2))
@@ -40,6 +60,16 @@ function mixed_volume(A::Matrix{Int}, V::Matrix{C}) where C
     return sum([vol(m, A) for m in cells])
 end
 
+"""
+    mixed_subdivision(A::Matrix{Int}, V::Matrix{C},
+                      d::Vector{Int} = rand(-LSIZE:LSIZE, size(A, 2)),
+                      d_start::Vector{Int} = rand(-LSIZE:LSIZE, size(A, 2))) where C
+
+Compute the mixed subdivision of the engineered complete intersection
+defined by `V` and `A` at the height vector `d` starting from the
+regular subdivision of `A` defined by `d_start`. This may fail if
+`d_start` is not sufficiently generic.
+"""
 function mixed_subdivision(A::Matrix{Int}, V::Matrix{C},
                            d::Vector{Int} = rand(-LSIZE:LSIZE, size(A, 2)),
                            d_start::Vector{Int} = rand(-LSIZE:LSIZE, size(A, 2))) where C
@@ -55,10 +85,28 @@ function mixed_subdivision(A::Matrix{Int}, V::Matrix{C},
     return p1[1:A_size], gather_mixed_cells(wd, A_size)
 end
 
-function real_root_count(A::Matrix{Int}, V::Matrix{QQFieldElem}, ms::Vector{MixedCellInds})
+"""
+    real_root_count(A::Matrix{Int}, V::Matrix{QQFieldElem},
+                    ms::Vector{MixedCellInds})
+
+Compute the real root count of `A`, `V` and the mixed subdivision given by `ms`.
+This is defined to be the sum of the real root counts of the polynomial systems
+corresponding to the leading parts of `A` and `V` indicated by `ms`.
+"""
+function real_root_count(A::Matrix{Int}, V::Matrix{QQFieldElem},
+                         ms::Vector{MixedCellInds})
+
     return sum([real_root_count(m, A, V) for m in ms])
 end
 
+"""
+    get_eliminant_polytope(F::Vector{<:MPolyRingElem})
+
+Compute the Newton polytope of the eliminant of the engineered
+complete intersection (A, V) where `A` is the support and `V` the
+coefficient matrix of `F`. The last k variables are eliminated where
+`F` consists of k + 1 equations.
+"""
 function get_eliminant_polytope(F::Vector{<:MPolyRingElem})
     A, V = get_eci_data(F)
     @info "setting up initial data"
